d32b3aa42ca32b0853e46eb9e71115a87350b595
Alf Fonti <alffonti@gmail.com>
2022-06-25 10:17:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const imgs = [
+    'images/img1.jpg',
+    'images/img2.jpg',
+    'images/img3.jpg'
+]
+
+const container = document.getElementById('container')
+
+function renderImages() {
+    let teamImages = ''
+    for (let i = 0; i < imgs.length; i++) {
+        teamImages += `<img class="team-img" src='${imgs[i]}'>`
+    }
+    container.innerHTML = teamImages
+}
+
+renderImages()
